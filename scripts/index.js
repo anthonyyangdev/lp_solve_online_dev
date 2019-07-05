4f530d07d7d40293ffc36a884e7df33fccc1d566
Anthony Yang <ay339@cornell.edu>
2019-07-06 06:24:08 +0900

    Documentation

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,14 @@
+// Sets whether the project is in development or in production
 const dev = true
 const LP_SOLVE_API = dev ? "http://localhost:5000/" : "https://peaceful-zion-91234.herokuapp.com/"
 
 
 $(document).ready(() => {
+
+  /**
+   * Constants in the website. HTML/CSS ids are stored as key => value mappings.
+   * HTML/CSS class names are stored in the {@var SYS.class} property.
+   */
   const SYS = {
     TEXTSPACE: '#textspace',
     SOURCE: '#source',
@@ -19,39 +25,88 @@ $(document).ready(() => {
     UPLOAD: '#upload',
     RUN: '#run',
     class: {
-      RESULT_TAB: '.result-tab',
-      TAB_BUTTON: '.tab-button'
+      RESULT_TABS: '.result-tab',
+      TAB_BUTTONS: '.tab-button'
     }
   }
+
+  /**
+   * Records the current state of the website
+   */
   let state = {
     current: SYS.SOURCE
   }
 
+  /**
+   * Updates the log with a new message. This message is to displayed following
+   * all of the previous log messages.
+   * @param {string} content The output to be displayed in the log.
+   */
   function updateLog(content) {
     $(SYS.LOG).val(`${$(SYS.LOG).val()}${content}\n`)
   }
 
+  /**
+   * Displays the text given an HTML/CSS id value onto the website textspace
+   * box.
+   * @param {string} element The id value of an HTML/CSS element.
+   */
   function displayTextIn(element) {
     $(SYS.TEXTSPACE).val($(element).val())
   }
 
+  /**
+   * Stores the content in the website textspace in the hidden value of an
+   * HTML/CSS element.
+   * @param {string} element The id value of an HTML/CSS element.
+   */
   function storeTextIn(element) {
     $(element).val($(SYS.TEXTSPACE).val())
   }
 
+  /**
+   * Changes the state of the website to the value of the next HTML/CSS 
+   * element id.
+   * @param {string} element The id value of an HTML/CSS element.
+   */
   function setNextStateAs(element) {
     state.current = element
   }
 
+  /**
+   * Returns the value stored in an HTML/CSS id.
+   * @param {string} element The id value of an HTML/CSS element.
+   */
   function valueOf(element) {
     return $(element).val()
   }
 
+  /**
+   * True if the current state is equal to an HTML/CSS element's id value. 
+   * False otherwise.
+   * @param {string} element The id value of an HTML/CSS element.
+   */
+  function currentStateIs(element) {
+    return element === state.current
+  }
+
+  /**
+   * Returns the id value of a pressed button event.
+   * @param {Event} e 
+   */
   function pressedButtonID(e) {
     const clicked_button = $(e.target)
     return `#${clicked_button[0].id}`
   }
 
+  /**
+   * When the upload button is clicked, users are prompted to select a file
+   * in their file systems. If a file is selected, then the contents of that
+   * file is displayed onto the website's text space.
+   * 
+   * The selected file must be a *.lp or a *.txt file. For any other files,
+   * the operation is cancelled.
+   */
   $(SYS.UPLOAD).click(() => {
     var input = document.createElement('input');
     input.type = 'file';
@@ -74,7 +129,12 @@ $(document).ready(() => {
     input.click();
   })
 
-
+  /**
+   * The values in {@var contents} are turned into files, where the filenames
+   * for each are their key values. These files are then downloaded into the 
+   * user's file systems.
+   * @param {Map<String, String>} contents 
+   */
   function downloadForUser(contents) {
     var a = document.createElement('a')
     for (var name in contents) {
@@ -90,28 +150,49 @@ $(document).ready(() => {
     }, 0)
   }
 
+  /**
+   * Download the report of a lp_solve calculation as one file named report.txt.
+   */
   $(SYS.DOWNLOAD_REPORT).click(() => {
     downloadForUser({ 'report.txt': valueOf(SYS.DOWNLOAD_REPORT) })
   })
 
+  /**
+   * Download the contents of the source text space as a file named lp_solve.txt
+   */
   $(SYS.DOWNLOAD).click(() => {
-    if (state.current === SYS.SOURCE) {
+    if (currentStateIs(SYS.SOURCE)) {
       storeTextIn(SYS.SOURCE)
     }
-    downloadForUser({
-      'lp_solve.txt': valueOf(SYS.SOURCE)
-    })
+    downloadForUser({ 'lp_solve.txt': valueOf(SYS.SOURCE) })
   })
 
+  /**
+   * Connects to the lp_solve_server, which performs the linear programming
+   * calculations, and receives the results from those calculations.
+   * 
+   * If there are no errors, then the log will message the user that the
+   * running is complete.
+   * Otherwise, the log message will display the error message preformed by 
+   * lp_solve.
+   * 
+   * If the website is unable to connect to the server, then log that the server
+   * could not be connected.
+   * 
+   * {@var response = {
+   *    report: [Object]
+   *    error: [String]
+   *    lp_solve_error: [String]
+   * }}
+   * 
+   */
   $(SYS.RUN).click(() => {
     // If currently on source, first save all current progress.
-    if (state.current === SYS.SOURCE) {
+    if (currentStateIs(SYS.SOURCE)) {
       storeTextIn(SYS.SOURCE)
     }
 
-    var request_data = {
-      content: $(SYS.SOURCE).val()
-    }
+    var request_data = { content: valueOf(SYS.SOURCE) }
 
     updateLog('Now Running...')
 
@@ -132,7 +213,8 @@ $(document).ready(() => {
       if (res.error === '') {
         updateLog('Running Complete.\n')
       } else {
-        updateLog(res.error)
+        updateLog(res.lp_solve_error)
+        return
       }
 
       var sections = res.report
@@ -147,20 +229,20 @@ $(document).ready(() => {
       }
       $(SYS.DOWNLOAD_REPORT).val(res.result)
       displayTextIn(state.current)
-    }).catch(function (e) {
-      console.log(e)
-      console.log('Error')
+    }).catch(function (_) {
+      updateLog('Unable to connect to the server...')
     })
   })
 
   /**
-   * Changes the content of the text area into the values associated to the button clicked.
+   * Changes the content of the text area into the values associated to the 
+   * button clicked.
    */
-  $(SYS.class.TAB_BUTTON).click((e) => {
+  $(SYS.class.TAB_BUTTONS).click((e) => {
     storeTextIn(state.current)
 
     setNextStateAs(pressedButtonID(e))
-    if (state.current === SYS.RESULT) {
+    if (currentStateIs(SYS.RESULT)) {
       $(SYS.RESULT_DASH).show()
       setNextStateAs(SYS.OBJECTIVE)
     } else {
@@ -168,11 +250,15 @@ $(document).ready(() => {
     }
     displayTextIn(state.current)
 
-    $(SYS.TEXTSPACE).attr('readonly', state.current !== SYS.SOURCE)
-    console.log('Assertion', state.current !== SYS.RESULT)
+    $(SYS.TEXTSPACE).attr('readonly', !currentStateIs(SYS.SOURCE))
+    console.log('Assertion', !currentStateIs(SYS.RESULT))
   })
 
-  $(SYS.class.RESULT_TAB).click(e => {
+  /**
+   * Changes the content of the text area into the values associated to the 
+   * button clicked.
+   */
+  $(SYS.class.RESULT_TABS).click(e => {
     var next_tab = pressedButtonID(e)
     storeTextIn(state.current)
     setNextStateAs(next_tab)
